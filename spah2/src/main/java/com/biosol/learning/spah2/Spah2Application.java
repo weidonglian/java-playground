a6c81f3a82647bb6fb6cfd85e66873abd6e8c863
Weidong Lian <weidong.lian@gmail.com>
2018-05-03 22:48:12 +0200

    init spah2

---
+++ spah2/src/main/java/com/biosol/learning/spah2/Spah2Application.java
@@ -0,0 +1,12 @@
+package com.biosol.learning.spah2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spah2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spah2Application.class, args);
+	}
+}
---
+++ spah2/src/test/java/com/biosol/learning/spah2/Spah2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.biosol.learning.spah2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spah2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
