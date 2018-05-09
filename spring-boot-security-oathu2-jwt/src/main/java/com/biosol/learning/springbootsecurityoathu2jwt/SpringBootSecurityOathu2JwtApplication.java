ca87280d5c0312998192400ef100de637769f64f
Weidong Lian <weidong.lian@gmail.com>
2018-05-10 00:08:03 +0200

    init spring-boot-security-oathu2-jwt project

---
+++ spring-boot-security-oathu2-jwt/src/main/java/com/biosol/learning/springbootsecurityoathu2jwt/SpringBootSecurityOathu2JwtApplication.java
@@ -0,0 +1,12 @@
+package com.biosol.learning.springbootsecurityoathu2jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityOathu2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityOathu2JwtApplication.class, args);
+	}
+}
